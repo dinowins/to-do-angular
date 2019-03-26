f5d2ad330a99dec304624bf9cc69d63efe5e51a6
Chris Breaux and Dino Nguyen <dino@>
2019-03-26 14:31:45 -0700

    Add edit and hide functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,8 +13,31 @@ export class AppComponent {
   day: number = this.currentTime.getDate();
   year: number = this.currentTime.getFullYear();
   tasks: Task[] = [
-    new Task('Finish weekend Angular homework for Epicodus course'),
-    new Task('Begin brainstorming possible JavaScript group projects'),
-    new Task('Add README file to last few Angular repos on GitHub')
+    new Task('Finish weekend Angular homework for Epicodus course', 3),
+    new Task('Begin brainstorming possible JavaScript group projects', 2),
+    new Task('Add README file to last few Angular repos on GitHub', 2)
   ];
+
+  selectedTask = null;
+  priorityColor(currentTask) {
+    if (currentTask.priority == 3) {
+      return "bg-danger";
+    } else if (currentTask.priority == 2) {
+      return "bg-warning";
+    } else {
+      return "bg-info";
+    }
+  }
+
+  editTask(clickedTask) {
+    console.log(clickedTask);
+    //method will accept a Task Object from the tasks array
+    //and set the selectedTask to that Object
+    this.selectedTask = clickedTask;
+  }
+
+  finishedEditing() {
+    //because the selectedTask is set to null, it won't show due to the *ngIf statemen
+    this.selectedTask = null;
+  }
 }
